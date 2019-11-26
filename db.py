07014a6fa154cbbcb57b78cf9b5d0d0c7e659e88
github <t.harms@mad-co.de>
2019-11-26 23:15:35 +0100

    save function and debug

--- db.py
+++ db.py
@@ -10,6 +10,7 @@ table_schema:
     `finish`	    INTEGER NOT NULL,
     `logs`	        TEXT NOT NULL,
     `status`	    INTEGER NOT NULL
+    `insertion`	    INTEGER NOT NULL
 '''
 class DB:
     __con = 0
@@ -24,7 +25,7 @@ class DB:
     # 9 some other error
     
     # helper list for building queries
-    __test_env_fields = ['testname', 'environment', 'start', 'finish', 'logs', 'status']
+    __test_env_fields = ['testname', 'environment', 'teststart', 'testfinish', 'logs', 'status']
 
     def __init__(self):
         self.__con = sqlite3.connect(self.__db_filename)
@@ -35,14 +36,15 @@ class DB:
 
     def execute_sql(self, sql):
         self.__cursor.execute(sql)
+        self.__con.commit()
 
     # expecting data_dict as dictionary of {attribute_name, value}
-    def save_article(self, data_dict):
+    def save_test(self, data_dict):
         count = 0
         attribute_names = data_dict.keys()
         max_count = len(attribute_names) - 1
 
-        sql = "INSERT INTO " + self.__table_tess + " ("
+        sql = "INSERT INTO " + self.__table_tests + " ("
 
         # build the chain of keys
         for key in attribute_names:
@@ -53,27 +55,44 @@ class DB:
                 count += 1
 
         # build intermezzo
-        sql = sql + " VALUES ("
+        sql = sql + " VALUES (\'"
 
         # build the chain of values
         count = 0
         for key in attribute_names:
 
             if count < max_count:
-                sql = sql + str(data_dict[key]) + ', '
+                sql = sql + str(data_dict[key]) + '\', \''
                 count += 1
             elif count == max_count:
-                sql = sql + str(data_dict[key]) + ') '
-
+                sql = sql + str(data_dict[key]) + '\') '
+        print(sql)
         # execute insertion
         self.__cursor.execute(sql)
+        #commit changes
+        self.__con.commit()
 
-'''
-def get_test_data(self):
+
+    def get_test_data(self):
         sql = "SELECT * FROM " + self.__table_tests
         self.__cursor.execute(sql)
         result_set = self.__cursor.fetchall()
         return result_set
+
+    def build_test_table(self):
+        sql = "CREATE TABLE `testenv` (\
+	        `id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,\
+            `testname`	TEXT NOT NULL,\
+            `environment`	TEXT NOT NULL,\
+            `start`	INTEGER NOT NULL,\
+            `finish`	INTEGER NOT NULL,\
+            `logs`	TEXT NOT NULL,\
+            `status`	INTEGER NOT NULL\
+        );"
+        return(sql)
 db = DB()
+#db.execute_sql(db.build_test_table())
+testdic = {'testname': 'testname1', 'environment': 'env1', 'start': 22, 'finish': 3324, 'logs': 'nologfile','status': 3}
+db.save_test(testdic)
 print(db.get_test_data())
-'''
+
