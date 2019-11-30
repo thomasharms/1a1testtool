677010abd406b9c4cec0f623171867a63f904773
github <t.harms@mad-co.de>
2019-11-30 22:46:44 +0100

    init changes, show last tests results

--- init.py
+++ init.py
@@ -1,19 +1,6 @@
 from db import DB
 import subprocess, os, sys
 
-def build_test_table():
-    db = DB()
-    sql = "CREATE TABLE `testenv` (\
-	        `id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,\
-            `testname`	TEXT NOT NULL,\
-            `environment`	TEXT NOT NULL,\
-            `start`	INTEGER NOT NULL,\
-            `finish`	INTEGER NOT NULL,\
-            `logs`	TEXT NOT NULL,\
-            `status`	INTEGER NOT NULL\
-        );"
-    db.execute_sql(sql)
-
 # fixed for three environments
 #  TODO implemented in a more flexible fashion regarding amounts of venvs
 def build_virtual_environments():
@@ -27,7 +14,14 @@ def install_pytest():
     subprocess.call([os.path.dirname(sys.argv[0])+'/venv2/bin/pip3', 'install', 'pytest'])
     subprocess.call([os.path.dirname(sys.argv[0])+'/venv3/bin/pip3', 'install', 'pytest'])
 
-#build_test_table()
+def build_db_table():
+    db = DB()
+    try:
+        db.build_test_table()
+    except Exception as e:
+        print(str(e))
+
+build_db_table()
 build_virtual_environments()
 install_pytest()
 
--- testresults.py
+++ testresults.py
@@ -6,15 +6,22 @@ result_count = 10
 def show_last_results(result_count):
         
     db = DB()
-    test_results = db.show_last_tests(result_count)
-    for i in range(len(test_results)):
-        print("\n *** Test Result Nr. %d ***\n" % i)
-        print("TEST NAME : %s" % test_results[i][0])
-        print("TEST ENVIRONMENT : %s" % test_results[i][1])
-        print("TEST START : " +str(datetime.fromtimestamp(test_results[i][2])))
-        print("TEST FINISH : " +str(datetime.fromtimestamp(test_results[i][3])))
-        print("TEST LOG : %s" % test_results[i][4])
-        print("TEST STATUS : %s" % test_results[i][5])
-        print("TEST INSERTION TIME : " +str(datetime.fromtimestamp(test_results[i][6])))
+    try:
+        test_results = db.show_last_tests(result_count)
+    except Exception as e:
+        print(str(e))
+
+    if type(test_results) is list and test_results:
+        for i in range(len(test_results)):
+            print("\n *** Test Result Nr. %d ***\n" % i)
+            print("TEST NAME : %s" % test_results[i][0])
+            print("TEST ENVIRONMENT : %s" % test_results[i][1])
+            print("TEST START : " +str(datetime.fromtimestamp(test_results[i][2])))
+            print("TEST FINISH : " +str(datetime.fromtimestamp(test_results[i][3])))
+            print("TEST LOG : %s" % test_results[i][4])
+            print("TEST STATUS : %s" % test_results[i][5])
+            print("TEST INSERTION TIME : " +str(datetime.fromtimestamp(test_results[i][6])))
+    else:
+        print("No tests done yet.")
 
 show_last_results(result_count)
